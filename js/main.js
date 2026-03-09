128f6ac385f19df130e3fd2c0467598b6ef0f06a
Kenny Richard-Tech Master <olugbusikehinde01@gmail.com>
2026-03-09 13:16:34 +0100

    my first javascript class

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// external javascript
+console.log("i am learning javascript");
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Congratulations";
+}
+let fname = "Kenny";
+let mname = " Olugbusi";
+let lname = " Richard";
+let fullname = fname + mname + lname;
+console.log(fullname);
+
+let x = 10;
+// x = 60;
+let y = 40;
+let z = x + y;
+// alert(z);
+
+const x1 = 100;
+// x1 = 400;
+console.log(x1);
+
+// typeof fname;
+console.log(typeof "kenny");
+console.log(typeof 2);
+console.log(typeof true);
+console.log(typeof 3.142);
